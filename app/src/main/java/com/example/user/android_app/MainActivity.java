f3a4546c8ddb5bf45be55f29f7d3d9fff9338d99
NAGARAJU401 <aadepu.nagaraj@gmail.com>
2017-12-01 11:37:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/android_app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.android_app;
+//Package objects contain version information about the implementation and specification of a Java package.
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Android class works in same.You are extending the Activity class which have onCreate(Bundle bundle) method in which meaningful code is written
+        //So to execute that code in our defined activity. You have to use super.onCreate(bundle)
+
+        setContentView(R.layout.activity_main);
+        //R means Resource
+        //layout means design
+        //  main is the xml you have created under res->layout->main.xml
+        //  Whenever you want to change your current Look of an Activity or when you move from one Activity to another .
+        // The other Activity must have a design to show . So we call this method in onCreate and this is the second statement to set
+        // the design
+    }
+}
